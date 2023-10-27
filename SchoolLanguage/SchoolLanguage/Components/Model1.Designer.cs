b01956ff03c5fe17e6da4f3728af99e1014d087d
1BlueSparkle1 <kukuruzum@mail.ru>
2023-10-27 13:21:51 +0300

    Added record conditions end 27.10

--- SchoolLanguage/SchoolLanguage/Components/Model1.Designer.cs
+++ SchoolLanguage/SchoolLanguage/Components/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\Acer\Source\Repos\LanguageSchool\SchoolLanguage\SchoolLanguage\Components\Model1.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\Acer\source\repos\LanguageSchool\SchoolLanguage\SchoolLanguage\Components\Model1.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
--- SchoolLanguage/SchoolLanguage/MyPages/AddEditServicePage.xaml.cs
+++ SchoolLanguage/SchoolLanguage/MyPages/AddEditServicePage.xaml.cs
@@ -46,12 +46,44 @@ namespace SchoolLanguage.MyPages
 
         private void SaveBtn_Click(object sender, RoutedEventArgs e)
         {
+            StringBuilder errors = new StringBuilder();
+
             if (service.ID == 0)
             {
-                App.db.Service.Add(service);
+                if (App.db.Service.Any(x => x.Title == service.Title))
+                {
+                    errors.AppendLine("Такая услуга уже существует!");
+                }
+                else
+                {
+                    App.db.Service.Add(service);
+                }
+
             }
-            App.db.SaveChanges();
+            if(service.DurationInSeconds > 14400)
+            {
+                errors.AppendLine("Длительность не может превышать 4 часов");
+            }
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+            }
+            else
+            {
+                App.db.SaveChanges();
+                MessageBox.Show("Сохранено");
+                navigation.NextPage(new PageComponent("Список услуг", new ServiceListPage()));
+            }
+
             
+        }
+
+        private void CostTb_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!(char.IsDigit(e.Text[0])))
+            {
+                e.Handled = true;
+            }
         }
     }
 }
